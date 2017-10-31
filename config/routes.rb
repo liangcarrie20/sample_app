81edfb741fdebe01fd71dda366882eeffb4f40a8
Carrie Liang <growify.carrie@gmail.com>
2017-10-30 22:19:35 -0700

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'application#hello'
   # root 'static_pages#home'
-
   get 'ccsf_rails/index'
-
   get 'ccsf_rails/links'
-
   get 'ccsf_rails/about'
   get 'ccsf_rails/assignments'
-
   get  'static_pages/home'
   get  'static_pages/help'
   get  'static_pages/about'
   get  'static_pages/contact'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
